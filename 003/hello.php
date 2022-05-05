192202cd633822102132ffa688308f7c18500cf1
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2022-05-05 11:16:19 +0900

    [chore]Webアプリから拝借(thks. shimat)

--- 003/hello.php
+++ 003/hello.php
@@ -1,21 +1,13 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="utf-8">
-<title>はろーわーるど</title>
+    <meta charset="UTF-8">
+    <title>はじめてのPHP</title>
 </head>
 <body>
-<?php
-    $now = intval((new Datetime('now'))->format('H'));
-    $message = "";
-    if(($now >= 0 && $now <= 7) || ($now >= 21 && $now <= 23) ) {
-        $message = "こんばんは";
-    } elseif($now >= 7 && $now <= 10) {
-        $message = "おはようございます";
-    } else { // 11〜19
-        $message = "こんにちは";
-    }
-?>
-ただいま <?= $now ?>時 です、<?= $message ?>。
+    <?php
+    echo 'Hello,PHP';
+    ?>
+    <p>0J○○□□□ 神戸　電子</p>
 </body>
 </html>
